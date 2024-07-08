30e2865f9ee1e4b6d0e14ce6370226b89eabdf37
upendraupi <upendrakuruma@gmail.com>
2024-07-08 11:27:47 +0530

    'authenticationchanges'

--- accounts/views.py
+++ accounts/views.py
@@ -2,34 +2,48 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-
 import csv
 from django.http import HttpResponse
 
 # Create your views here.
-def Signpage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        user = User.objects.create_user(username=username,email=email,password=password)
-        user.save()
-        messages.success(request,'Your Acount has been created successfully')
-        return redirect('Login')
-    return render(request,'accounts/signup.html')
+def Sign_page(request):
+ if request.method == 'POST':
+  username = request.POST.get('username')
+  email = request.POST.get('email')
+  password1 = request.POST.get('password1')
+  password2 = request.POST.get('password2')
+  if password1 == password2:
+   if User.objects.filter(username=username).exists():
+    messages.info(request,"Username Token")
+    return render(request, 'accounts/signup.html')
+   elif User.objects.filter(email=email).exists():
+    messages.info(request,"Email Token")
+    return render(request, 'accounts/signup.html')
+   else:
+      user = User.objects.create_user(username=email,first_name=username,email=email,password=password1)
+      user.save()
+      messages.success(request,"Your account has been Register Successfully!")
+      return redirect("Login")
+  else:
+   messages.info(request,"Password Not Matched")
+   return render(request, 'accounts/signup.html')
+ else:
+   return render(request, 'accounts/signup.html')
 
-def Loginpage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate (username=username,password=password)
-        if user is not None:
-            login(request,user)
-            messages.success(request,'You are successfully login ')
-            return redirect('Blogpage')
-        else:
-            messages.warning(request,'Invalid creadential found ')
-    return render(request,'accounts/login.html')
+def Login_page(request):
+ if request.method == 'POST':
+  username = request.POST.get('email')
+  password = request.POST.get('password')
+  user = authenticate(username=username,password=password)
+  if user is not None:
+   login(request,user)
+   messages.info(request,'Successfully Login')
+   return redirect('Blogpage')
+  else:
+   messages.info(request,'User name or Password not Match')
+   return render(request, 'accounts/login.html')
+ else:
+   return render(request, 'accounts/login.html')
 
 def logout_user(request):
     messages.success(request,'Your Successfully Logout ')
